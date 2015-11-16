c5d464e80e2129cb6e3f47bfd88bd8a93a3b6399
connorshea <connor.james.shea@gmail.com>
2015-11-16 10:16:27 -0700

    P10 updates and CS192 rock-paper-scissors python game

--- CS160/P10/src/P10.java
+++ CS160/P10/src/P10.java
@@ -45,19 +45,35 @@ public class P10 implements ImageInterface {
 
   @Override
   public void decode() {
-      // Calling decode restores an image in which each pixel has had the upper four bits negated. To unscramble the image, your code should negate them again. You can use bitwise operators or the following algorithm: To get the upper bits, divide the pixel by 16. To get the lower bits, modulo the pixel by 16. The resulting upper and lower values should be in the range 0..15. Negate the upper bits as follows: upper = 15 - upper; Then put the bits back together by multiplying the upper bits by 16 and adding the result to lower. Here's an example for your testing:
-      // original pixel = 115 = 0b01110011
-      // upper value = 115 / 16 = 7 = 0b0111 (upper four bits of original)
-      // lower value = 115 % 16 = 3 = 0b0011 (lower four bits of original)
-      // negate upper value = 15 - 7 = 8 = 0b1000
-      // new pixel = (upper * 16) + lower = (8 * 16) + 3 = 131 = 0b10000011
+      // Calling decode restores an image in which each pixel has had the upper 
+      // four bits negated. To unscramble the image, your code should negate them 
+      // again. You can use bitwise operators or the following algorithm: To get 
+      // the upper bits, divide the pixel by 16. To get the lower bits, modulo the 
+      // pixel by 16. The resulting upper and lower values should be in the range 
+      // 0..15. Negate the upper bits as follows: upper = 15 - upper; Then put the 
+      // bits back together by multiplying the upper bits by 16 and adding the 
+      // result to lower. Here's an example for your testing:
+      //   original pixel = 115 = 0b01110011
+      //   upper value = 115 / 16 = 7 = 0b0111 (upper four bits of original)
+      //   lower value = 115 % 16 = 3 = 0b0011 (lower four bits of original)
+      //   negate upper value = 15 - 7 = 8 = 0b1000
+      //   new pixel = (upper * 16) + lower = (8 * 16) + 3 = 131 = 0b10000011
 
 
   }
 
   @Override
   public void swap() {
-	  // TODO Auto-generated method stub
+	  // Calling swap restores an image in which each pixel has been scrambled by 
+    // exhanging the lower 3 bits with the upper 3 bits. To do this requires that 
+    // your code do the same exchange to restore the original pixel. Don't modify 
+    // the middle two bits. By far the easiest way to do this is to use the 
+    // bitwise operators. Here's an example for your testing:
+    //   original pixel = 114 = 0b01110010
+    //   upper three bits of original = 0b01110010 & 0b11100000 = 0b01100000
+    //   middle two bits of original = 0b01110010 & 0b00011000 = 0b00010000
+    //   lower three bits of original = 0b01110010 & 0b00000111 = 0b00000010
+    //   new pixel = (lower << 5) + middle + (upper >> 5)
 
   }
 
---
+++ CS192/rock-paper-scissors/rps_back.py
@@ -0,0 +1,135 @@
+import random
+import matplotlib.pyplot as plt
+import rps
+
+
+def instructions(response):
+    # Add to instruction_string so that it contains information on
+    # how to play rock-paper-scissors
+    instruction_string = "Rock beats scissors. Scissors beats paper. Paper beats rock. If understood 'yes' if not 'no'."
+
+    # Use a string method to make response all one case
+    answer = input()
+    # Use an if statement to check if the response is "yes"
+    if lower(answer) == "yes":
+        rps.print_instructions(instruction_string)
+
+
+
+# Name requirements: must be at least two characters, must be no more than 10 characters, must only be 
+# one name (i.e. no spaces), must start with an upper case letter.
+# Return requirement: name
+def check_name(name):
+
+    error = "Somethings wrong"
+
+    if len(name) < 11:
+        rps.quit_game(error)
+    if len(name) > 1:
+        rps.quit_game(error)
+    if name.find(' ') == true:
+        rps.quit_game(error)
+    if name.istitle() == false:
+        rps.quit_game(error)
+
+    # Use if statements, as above, to check if 'name' meets the requirements listed above. If it fails
+    # any condition call rps.quit_game with an appropriate error message.
+
+    # return the variable name once have checked all name requirements
+    return name
+
+
+
+
+
+# Num requirements: must be more than two and less than 21.
+# Return requirement: num
+def check_times_to_play(num):
+    error = "Number is not correct"
+
+    # Check that 'num' meets the requirements above.
+
+    if (2 < int(num)) and (int(num) < 21):
+        return num
+    else:
+        rps.quit_game(error)
+
+    # If 'num' does not meet requirements call rps.quit_game with an error
+    # message (the variable 'error.')
+    # Note: use function int, to convert num to integer when using it to compare to another integer, i.e. int(num)
+
+    # return the variable num once have checked requirements
+
+
+
+
+
+def play_game(name):
+    # This gets the move from a player when they push a button.
+    player_move = rps.get_player_move()
+
+    # Use the random library to chose a random move for the player.
+    random_moves = ['rock', 'paper', 'scissors']
+    string_choice = random.choice(random_moves)
+
+    if (player_move == string_choice):
+        who_won = "Tie"
+    elif ((player_move == 'rock') and (string_choice == 'paper')):
+        who_won = "Computer"
+    elif ((player_move == 'paper') and (string_choice == 'scissors')):
+        who_won = "Computer"
+
+    # After determines who won, build a results string.
+    # The next 3 lines partially builds this string. Complete for computer, add for tie, and add line for winner
+    results = "Player played " + player_move
+    results += "\n"
+    results += "Computer played "
+
+    # Use rps.display_results to display the results string for the game.
+    rps.display_results
+
+    # return the variable who_won
+    return who_won
+
+
+def play_match():
+
+    # Call rps.ask_instructions() to see if player wants instructions
+    rps.ask_instructions()
+
+    # Call rps.get_name() to get a string of the players name
+    # Be sure to save return value, i.e. player_name = rps.get_name()
+    player_name = rps.get_name()
+
+
+    # Call rps.get_num_play to get the number of games to play
+    # Be sure to save return value, i.e. num_times = rps.get_num_play()
+    num_times = rps.get_num_play()
+
+    
+    # Use these variables to keep track of who won
+    ties = 0
+    player_wins = 0
+    computer_wins = 0
+
+
+    # Use a while or for loop to call play_game the correct amount of times
+    count = 0
+    while count < num_times:
+        game_winner = play_game(name)
+        # Now use an if statement to increment total for winner
+        # i.e. will increment either ties, player_wins, or computer_winds
+        count = count + 1
+
+    # Call make_graph using the variables for win counts above.
+    make_graph(player_name, player_wins, computer_wins, ties)
+
+
+
+
+# Create a graph as we did in the previous lab
+def make_graph(name, player_wins, comp_wins, ties):
+    print "Not implemented yet"
+
+
+
