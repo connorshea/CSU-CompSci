cc3c3dc44fe2e9aab55279365e2a75a7b34e6740
connorshea <connor.james.shea@gmail.com>
2015-11-17 10:40:33 -0700

    R18

---
+++ CS160/R18/src/Music.java
@@ -0,0 +1,14 @@
+// R18 Assignment
+// Author: Connor Shea
+// Date:   11/18/2015
+// Class:  CS160
+// Email:  coshea@cs.colostate.edu
+// http://www.cs.colostate.edu/~cs160/.Fall15/recitations/R18.html
+
+public class Music {
+	 private String songTitle;
+	 private String albumName;
+	 private String artistName;
+	 private int releaseYear;
+	    
+}
